7a34bc347a2cdde4e7d2ec2a98f9145cb64f1cae
DeNaice <dennis.krs@hotmail.de>
2024-05-02 08:15:35 +0200

    Upload to work on Laptop

---
+++ ownPlugin/ownPlugin.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: ownPlugin  
+Description: Mein selbsterstelltes Plugin
+Version: 1.0.0
+Author: Dennis Krs
+Author URI: localhost
+*/
+
+namespace THM\ownPlugin;
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+require_once(dirname(__FILE__) . '/modules/main.php');
+
+?>
